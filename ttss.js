6f7ade06062f6ba1dac12099cf645335d32956f9
Nabilahfzh17 <74862546+Nabilahfzh17@users.noreply.github.com>
2021-10-30 22:16:00 +0700

    Add files via upload

---
+++ ttss.js
@@ -0,0 +1,39 @@
+var ttss = [
+	{
+		"word":"video",
+		"clue":"Format Avi digunakan untuk"
+	},
+	{
+		"word":"bootstrap",
+		"clue":"Framework open source khusus untuk front-end berbasis html dan css"
+	},
+	{
+		"word":"fontcolor",
+		"clue":"Untuk mengganti warna dari tulisan yang akan ditampilkan dalam Label atau Text Box digunakan property"
+	},
+	{
+		"word":"domain",
+		"clue":"Nama unik yang diberikan untuk mengidentifikasi nama server komputer seperti web server atau email server di jaringan komputer ataupun internet adalah"
+	},
+	{
+		"word":"hitam",
+		"clue":"Kode #000000 merupakan kode warna.."
+	},
+	{
+		"word":"body",
+		"clue":"Struktur HTML adalah head dan .... "
+	},
+	{
+		"word":"browser",
+		"clue":"Aplikasi/software yg digunakan untuk mengakses WWW di internet disebut…"
+	},
+	{
+		"word":"javascript",
+		"clue":"Salah satu bahasa pemrograman web yang bersifat Client Side Programming Language"
+	},
+	{
+		"word":"sheet",
+		"clue":"CSS adalah singkatan dari Cascading Style ..... "
+	},
+	
+]
